a90837956c83902a6df46a617ff8ea4dbaf8790e
KELVINEITOR458 <kelvinbermeo8@gmail.com>
2024-03-10 17:17:52 -0500

    todas las funciones fueron creadas

---
+++ Clases/src/com/krakedev/Calculadora.java
@@ -0,0 +1,33 @@
+package com.krakedev;
+
+public class Calculadora {
+	public int sumar(int valor1, int valor2) {
+		int resultado;//declarar la variable
+		resultado = valor1 + valor2;
+		return resultado;
+	}
+	
+	public double restar(int valor1, int valor2) {
+		double resultado;
+		resultado = valor1 - valor2;
+		return resultado;
+	}
+	
+	public double multiplicar(double valor1, double valor2) {
+		double resultado;
+		resultado = valor1 * valor2;
+		return resultado;
+	}
+	
+	public double dividir(double dividendo, double divisor) {
+		double resultado;
+		resultado = dividendo / divisor;
+		return resultado;
+	}
+	
+	public double promediar(double nota1, double nota2, double nota3) {
+		double resultado;
+		resultado = (nota1 + nota2 + nota3)/3;
+		return resultado;
+	}
+}
---
+++ Clases/src/com/krakedev/TestCalculadora.java
@@ -0,0 +1,37 @@
+package com.krakedev;
+
+public class TestCalculadora {
+
+	public static void main(String[] args) {
+		Calculadora calcu;
+		int resultadoSuma;
+		calcu = new Calculadora();
+		resultadoSuma = calcu.sumar(5, 3);
+		System.out.println("La suma dió: "+ resultadoSuma);
+		
+		Calculadora rest;
+		double resultadoResta;
+		rest = new Calculadora();
+		resultadoResta = rest.restar(10, 3);
+		System.out.println("La resta dió: " + resultadoResta);
+		
+		Calculadora multi;
+		double resultadoMulti;
+		multi = new Calculadora();
+		resultadoMulti = multi.multiplicar(10, 5);
+		System.out.println("La multiplicación dió: " + resultadoMulti);
+		
+		Calculadora div;
+		double resultadoDiv;
+		div = new Calculadora();
+		resultadoDiv = div.dividir(10, 2);
+		System.out.println("La división dió: " + resultadoDiv);
+		
+		Calculadora prom;
+		double promedio;
+		prom = new Calculadora();
+		promedio = prom.promediar(10, 8, 9);
+		System.out.println("El promedio es: " + promedio);
+	}
+
+}
